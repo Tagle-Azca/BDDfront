11abe38558850253ba36b03dfe7c24187ce18256
EskayserDev <eskayser@gmail.com>
2025-05-02 17:32:56 -0600

    rutas backend

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./assets/pages/Login";
 import FraccDashboard from "./assets/pages/Dashboard";
 import AdminDashboard from "./assets/pages/Admin";
+import Invitados from "./assets/pages/Invitados"
+
 function App() {
   return (
     <Router>
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/dashboard/:id" element={<FraccDashboard />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/Visitas" element={<Invitados />} />
       </Routes>
     </Router>
   );
